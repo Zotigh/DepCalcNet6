9ed57658ee2e6df9bec3504ad159e7fc7e0ef0d4
Zotigh <lzotigh1@cnm.edu>
2022-07-10 18:44:52 -0600

    Page is working added the singleton service to the program but there are now errors when trying to submit a calculation. Also the date picker is not working.

--- DepCalcNet6/Program.cs
+++ DepCalcNet6/Program.cs
@@ -1,11 +1,15 @@
 using DepCalcNet6;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using DepCalcNet6.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<SuccessfullyCalculatedResults>();
 
 await builder.Build().RunAsync();
