e23341913741b2a93ea2551a3da42f325273cc1e
Zotigh <lzotigh1@cnm.edu>
2022-07-31 22:03:01 -0600

    Added comments of what needs to be done.

--- DepCalcNet6/Models/DoubleDecliningDeprication.cs
+++ DepCalcNet6/Models/DoubleDecliningDeprication.cs
@@ -2,5 +2,9 @@
 {
     public class DoubleDecliningDeprication
     {
+        //TODO add a double line deprication calculation method
+        //Equation for double declining method Deprication = 2 x Straightline Deprication Rate x Book Value at the beginning of the year.
+        //Periodic Deprication Expense = Beginning book value * rate of deprication Might have to divide by 1 same as above so I can get a correct answer.
+        // DoubleDeclining = 2 * endValue x startValue;
     }
 }
