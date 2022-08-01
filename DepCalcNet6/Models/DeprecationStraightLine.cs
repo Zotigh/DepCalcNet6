ea0ef2033e4357b2622bcc239368ba9bc3c9c688
Zotigh <lzotigh1@cnm.edu>
2022-07-31 21:59:58 -0600

    Added the equation but will most likely need to make another class that will inherit this class to make the final calculation.

--- DepCalcNet6/Models/DeprecationStraightLine.cs
+++ DepCalcNet6/Models/DeprecationStraightLine.cs
@@ -97,6 +97,7 @@ namespace DepCalcNet6.Models
             //TODO add a double line deprication calculation method
             //Equation for double declining method Deprication = 2 x Straightline Deprication Rate x Book Value at the beginning of the year.
             //Periodic Deprication Expense = Beginning book value * rate of deprication Might have to divide by 1 same as above so I can get a correct answer.
+            // DoubleDeclining = 2 * endValue x startValue;
             // I might also try and add Units of productions and Sum of years digits but need to read up on the subject a bit more.
         }
     }
