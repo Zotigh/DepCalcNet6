585390310c4f03115cb468eaff175bcc93ddc553
Zotigh <lzotigh1@cnm.edu>
2022-07-31 21:57:48 -0600

    Added base for the eqyatin that will be used

--- DepCalcNet6/Models/DeprecationStraightLine.cs
+++ DepCalcNet6/Models/DeprecationStraightLine.cs
@@ -95,6 +95,7 @@ namespace DepCalcNet6.Models
                 salvageValue) * (1 / lifeTime);
 
             //TODO add a double line deprication calculation method
+            //Equation for double declining method Deprication = 2 x Straightline Deprication Rate x Book Value at the beginning of the year.
             //Periodic Deprication Expense = Beginning book value * rate of deprication Might have to divide by 1 same as above so I can get a correct answer.
             // I might also try and add Units of productions and Sum of years digits but need to read up on the subject a bit more.
         }
